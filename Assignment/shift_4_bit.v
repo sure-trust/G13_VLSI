c5eeda0edf19423e7fdfc9db48582edb9d66b89c
rakshithk895 <127726939+rakshithk895@users.noreply.github.com>
2024-01-20 22:16:02 +0530

    Delete Assignment directory

--- Assignment/shift_4_bit.v
+++
@@ -1,9 +0,0 @@
-module top(Pi,Psi,Pclk,Pse,Po,Pso);
- input Pi,Pclk,Pse,Psi;
- output Po,Pso;
- sdff f1(D(Pi),.Si(Psi),.Se(Pse),.Clk(Pclk),.Q(w1));
-
- dff f2(D(w1),.clk(Pclk),Q(w2));
- dff f3(D(w2),.Clk(Pclk),.Q(w3));
- sdff f4(D(w3),.Si(w3),.Se(Pse),.Clk(Pclk),.Q(Po),So(Pso));
- endmodule
