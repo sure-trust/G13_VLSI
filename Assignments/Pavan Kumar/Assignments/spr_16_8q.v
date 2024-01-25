567b966ccab709bce812df69fbe770666db006fd
PAVAN KUMAR S <Pavanrajseetha9937@gmail.com>
2024-01-25 19:33:05 +0530

    Update spr_16_8q.v

--- Assignments/Pavan Kumar/Assignments/spr_16_8q.v
+++ Assignments/Pavan Kumar/Assignments/spr_16_8q.v
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module spr_16_8q(d,clk,q,a,wren);
 input [7:0] d;
 input [4:0] a;
@@ -15,23 +14,3 @@ end
 
 endmodule
 
-=======
-module spr_16_8q(d,clk,q,a,wen);
-input [7:0] d;
-input [4:0] a;
-output  reg [7:0] q;
-input clk,wen;
-
-reg [7:0] mem[15:0];
-
-always@(posedge clk)
-begin
-if(wen==1)
-mem[a]<=d;
-else
-q<=mem[a];
-end
-
-endmodule
-
->>>>>>> 3c2e907bbf82f92cca2cae619c53e66e0f59ff7d
